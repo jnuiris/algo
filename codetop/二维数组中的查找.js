9fa7e53efe805076dfd16b757fe78d2c93847daa
jnuiris <jnuzzb@gmail.com>
2021-06-02 16:35:53 +0800

    第一次提交

---
+++ codetop/二维数组中的查找.js
@@ -0,0 +1,14 @@
+var findNumberIn2DArray = function(matrix, target) {
+    let x = matrix.length - 1;
+    let y = 0;
+    while(x >= 0 && y < matrix[0].length) {
+        if(target > matrix[x][y]) {
+            y++;
+        }else if(target < matrix[x][y]) {
+            x--;
+        }else {
+            return true;
+        }
+    }
+    return false;
+};
