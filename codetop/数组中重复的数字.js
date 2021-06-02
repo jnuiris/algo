8bd11ff5bec3b69763b4fdd9cf3c0f6d82ee18e9
jnuiris <jnuzzb@gmail.com>
2021-06-02 16:25:44 +0800

    原地交换

--- codetop/数组中重复的数字.js
+++ codetop/数组中重复的数字.js
@@ -1,11 +1,27 @@
 //  方法1 set
+// var findRepeatNumber = function(nums) {
+//     let s = new Set();
+//     for(let num of nums) {
+//         if(!s.has(num)) {
+//             s.add(num);
+//         }else {
+//             return num;
+//         }
+//     }
+// };
+//
+// console.log(findRepeatNumber([2, 3, 1, 0, 2, 5, 3]));
+
+//  方法2 原地交换
 var findRepeatNumber = function(nums) {
-    let s = new Set();
-    for(let num of nums) {
-        if(!s.has(num)) {
-            s.add(num);
-        }else {
-            return num;
+    for(let i = 0; i < nums.length; i++) {
+        if(nums[i] != i) {
+            let tmp = nums[i];
+            nums[i] = nums[tmp];
+            nums[tmp] = tmp;
+            if(nums[i] == nums[nums[i]]) {
+                return nums[i];
+            }
         }
     }
 };
