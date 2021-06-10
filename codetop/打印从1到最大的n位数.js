9f9c0638ef50125d46302fdc29305fafcd87dace
jnuiris <jnuzzb@gmail.com>
2021-06-10 17:16:35 +0800

    第一次提交

---
+++ codetop/打印从1到最大的n位数.js
@@ -0,0 +1,10 @@
+var printNumbers = function(n) {
+    let num = Math.pow(10, n);
+    let arr = [];
+    for(let i = 1; i < num; i++) {
+        arr.push(i);
+    }
+    return arr;
+};
+
+printNumbers(1);
